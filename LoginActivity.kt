2c4b928200c76ac10f31be1c86ad9c8431ac54be
seungyongC <likerberi97@gmail.com>
2019-10-06 16:49:50 +0900

    Added kakaoLogin based on kotlin

--- LoginActivity.kt
+++ LoginActivity.kt
@@ -1,18 +1,24 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.kakao.auth.ISessionCallback
+import com.kakao.auth.Session
+import com.kakao.util.exception.KakaoException
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
-    private lateinit var auth: FirebaseAuth;
+    private lateinit var kakaoCallback: ISessionCallback
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +31,15 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
         val loading = findViewById<ProgressBar>(R.id.loading)
 
         auth = FirebaseAuth.getInstance()
+        kakaoCallback = SessionCallback()
+        Session.getCurrentSession().addCallback(kakaoCallback)
+        Session.getCurrentSession().checkAndImplicitOpen()
+
         // TODO Providers vs. Users.
+        // PLButton.isEnabled() to click or enable.
+
+        PLButton.isEnabled = true
+        CLButton.isEnabled = true
 
         PLButton.setOnClickListener(this)
         CLButton.setOnClickListener (this)
@@ -35,35 +49,56 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
         if (!validateForm()) {
             return
         }
-
+        auth = FirebaseAuth.getInstance()
         // [START create_user_with_email]
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
-
-                    val user = auth.currentUser
-
+                    val user = auth.currentUser?.email
+                    Toast.makeText(
+                        applicationContext,
+                        user,
+                        Toast.LENGTH_LONG
+                    ).show()
+                    val nextIntent = Intent(this, ProviderActivity::class.java)
+                    startActivity(nextIntent)
                 } else {
                     // If sign in fails, display a message to the user.
+                    Toast.makeText(
+                        applicationContext,
+                        "create_error",
+                        Toast.LENGTH_LONG
+                    ).show()
                 }
             }
-
     }
 
     private fun signIn(email: String, password: String) {
         if (!validateForm()) {
             return
         }
+        auth = FirebaseAuth.getInstance()
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
-
-                    val user = auth.currentUser
-
+                    val user = auth.currentUser?.email
+                    // displayName is nulll...........
+                    Toast.makeText(
+                        applicationContext,
+                        user,
+                        Toast.LENGTH_LONG
+                    ).show()
+                    val nextIntent = Intent(this, ProviderActivity::class.java)
+                    startActivity(nextIntent)
                 } else {
                     // If sign in fails, display a message to the user
+                    Toast.makeText(
+                        applicationContext,
+                        "signin_error",
+                        Toast.LENGTH_LONG
+                    ).show()
                 }
             }
     }
@@ -93,9 +128,37 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View) {
         val id = v.id
         // TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        if (!validateForm()) {
+            return
+        }
+
         when(id) {
             R.id.PLButton -> createAccount(username.text.toString(), password.text.toString())
             R.id.CLButton -> signIn(username.text.toString(), password.text.toString())
         }
+    }
+
+    inner class SessionCallback : ISessionCallback {
+        override fun onSessionOpenFailed(exception: KakaoException?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onSessionOpened() {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            //
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if ( Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
+            return
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Session.getCurrentSession().removeCallback(kakaoCallback)
     }
 }
